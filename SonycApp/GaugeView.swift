0aac59aee3e13614130fedd7f9d0785a4e62716d
Vanessa Johnson <vmj232@nyu.edu>
2020-07-23 16:13:09 -0400

    first commit

---
+++ SonycApp/GaugeView.swift
@@ -0,0 +1,21 @@
+//
+//  GaugeView.swift
+//  SonycApp
+//
+//  Created by Vanessa Johnson on 7/23/20.
+//  Copyright © 2020 Vanessa Johnson. All rights reserved.
+//
+
+import UIKit
+
+class GaugeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SonycApp/MapView.swift
@@ -0,0 +1,9 @@
+//
+//  MapView.swift
+//  SonycApp
+//
+//  Created by Vanessa Johnson on 7/23/20.
+//  Copyright © 2020 Vanessa Johnson. All rights reserved.
+//
+
+import Foundation
