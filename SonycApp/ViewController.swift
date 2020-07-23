afde646ded363c7cd25995e1a0024aa64dcce203
Vanessa Johnson <vmj232@nyu.edu>
2020-07-23 00:51:41 -0400

    Initial Commit

---
+++ SonycApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SonycApp
+//
+//  Created by Vanessa Johnson on 7/23/20.
+//  Copyright © 2020 Vanessa Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
