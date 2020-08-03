afbff67430737e5d6c6c04634ff326ea972b846f
Vanessa Johnson <vmj232@nyu.edu>
2020-08-03 16:16:27 -0400

    adding filters&conversions

--- SonycApp/Filters&Conversions.swift
+++ SonycApp/Filters&Conversions.swift
@@ -11,7 +11,7 @@ import Accelerate
 import AudioToolbox
 import AVFoundation
 
-
+//credit to https://developer.apple.com/documentation/accelerate/equalizing_audio_with_vdsp for the 6 values below
 var biquadFilter: vDSP.Biquad<Float>?
 
 let forwardDCT = vDSP.DCT(count:  bufferSize,
@@ -23,102 +23,117 @@ let inverseDCT = vDSP.DCT(count: bufferSize,
 
 
 var forwardDCT_PreProcessed = [Float](repeating: 0,
-                                           count: bufferSize)
+                                      count: bufferSize)
 
 var forwardDCT_PostProcessed = [Float](repeating: 0,
-                                            count: bufferSize)
+                                       count: bufferSize)
 
 var inverseDCT_Result = [Float](repeating: 0,
-                              count: bufferSize)
+                                count: bufferSize)
 
 class FiltersConversions: UIViewController{
     
 }
 
+
+//applies the a-weighted filter
+//this function up to the array called values, will be credited to: https://developer.apple.com/documentation/accelerate/equalizing_audio_with_vdsp
+//takes in a float array and returns a float array. (takes in the audio sample array)
 func apply(dctMultiplier: [Float], toInput input: [Float]) -> [Float] {
-       // Perform forward DCT.
-       forwardDCT?.transform(input,
-                             result: &forwardDCT_PreProcessed)
-       // Multiply frequency-domain data by `dctMultiplier`.
-       vDSP.multiply(dctMultiplier,
-                     forwardDCT_PreProcessed,
-                     result: &forwardDCT_PostProcessed)
-       
-       // Perform inverse DCT.
-       inverseDCT?.transform(forwardDCT_PostProcessed,
-                             result: &inverseDCT_Result)
-       
-       // In-place scale inverse DCT result by n / 2.
-       // Output samples are now in range -1...+1
-       vDSP.divide(inverseDCT_Result,
-                   Float(bufferSize/2),
-                   result: &inverseDCT_Result)
+    // Perform forward DCT.
+    forwardDCT?.transform(input,
+                          result: &forwardDCT_PreProcessed)
+    // Multiply frequency-domain data by `dctMultiplier`.
+    vDSP.multiply(dctMultiplier,
+                  forwardDCT_PreProcessed,
+                  result: &forwardDCT_PostProcessed)
     
-       var values: [Float] = inverseDCT_Result
-        values.enumerated().forEach{ index, value in
-            values[index] = powf(value, 2.0)
-        }
+    // Perform inverse DCT.
+    inverseDCT?.transform(forwardDCT_PostProcessed,
+                          result: &inverseDCT_Result)
     
+    // In-place scale inverse DCT result by n / 2.
+    // Output samples are now in range -1...+1
+    vDSP.divide(inverseDCT_Result,
+                Float(bufferSize/2),
+                result: &inverseDCT_Result)
     
+    //a new array that has the squares all the values in the audio sample array to make sure they are all positive later on
+    var values: [Float] = inverseDCT_Result
+    values.enumerated().forEach{ index, value in
+        values[index] = powf(value, 2.0)
+    }
+    
+    //a new array that has the square root values of the squared ones beforehand^
     var values1: [Float] = values
-          values1.enumerated().forEach{ index, value in
-              values1[index] = sqrtf(value)
-          }
+    values1.enumerated().forEach{ index, value in
+        values1[index] = sqrtf(value)
+    }
     
-   
     
+    // a new array that calculates the log of the previous values.
     var values2: [Float] = values1
-             values2.enumerated().forEach{ index, value in
-                 values2[index] = log10(value)
-             }
-    
+    values2.enumerated().forEach{ index, value in
+        values2[index] = log10(value)
+    }
+    //returns there values
     return values2
-   }
+}
 
+//gets the average of the decibels
+//takes in a float array and returns an int, the average of the array
 func applyMean(toInput input: [Float]) -> Int{
-
+    
+    //adds up the entered array
     let sumArray = input.reduce(0, +)
-//    print("The sumArray is")
-//    print(sumArray)
+    
+    //calculates the average of the array by dividing the sum by the amount of values in the inputted array
     let avgArrayValue = sumArray / Float(input.count)
+    //converts to integer
     let intAvgArrayValue = Int(avgArrayValue)
-    print(intAvgArrayValue)
+    
     return intAvgArrayValue
+    
 }
 
 
-
+//finishes the calculation of the decibels (sound pressure levels)
+//takes in a float array and returns a float array
 func decibelsConvert(array: [Float]) -> [Float]{
-        var dbs: [Float] = array
-        dbs.enumerated().forEach{ index, value in
-             dbs[index] =  20 * value
-//            dbs[index] =  20 * (log10(sqrtf(value)))
-        }
     
+    //array that stores value from the inputted array multipled by 20
+    var dbs: [Float] = array
+    dbs.enumerated().forEach{ index, value in
+        dbs[index] =  20 * value
+    }
+    
+    //array that adds the offset to each value from the previous array (dbs)
     var dbs1: [Float] = dbs
-     dbs1.enumerated().forEach{ index, value in
+    dbs1.enumerated().forEach{ index, value in
         dbs1[index] =  value + Float(calibrationOffset)
-    //            dbs[index] =  20 * (log10(sqrtf(value)))
-            }
+        
+    }
     
+    //absolute value of each value from previous array (dbs1)
     var dbs3: [Float] = dbs1
-             dbs3.enumerated().forEach{ index, value in
-                dbs3[index] =  abs(value)
-                    }
-
+    dbs3.enumerated().forEach{ index, value in
+        dbs3[index] =  abs(value)
+    }
     
-    
-
-    print(dbs3)
     return dbs3
-//    print(dbs2)
-//    return dbs2
+    
 }
 
+//takes in a float array and returns a single float
 func getMin(array: [Float]) -> Float{
-    return array.min()!
+    let small = array.min()!
+    if(array.min()! < small){
+        let small = array.min()!
+    }
+    return small
 }
 
+//takes in a float array and returns a single float
 func getMax(array: [Float]) -> Float{
     return array.max()!
 }
